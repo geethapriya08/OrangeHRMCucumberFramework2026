e2a870ae1259ef6a811b367c2372d5fdeec4b5fb
geethapriya08 <y.geethapriya@gmail.com>
2026-04-09 15:17:19 +0530

    Signed-off-by: geethapriya08 <y.geethapriya@gmail.com>

--- src/test/java/com/orangeHRM/runners/OrangeHrmMasterRunner.java
+++ src/test/java/com/orangeHRM/runners/OrangeHrmMasterRunner.java
@@ -21,7 +21,6 @@ import io.cucumber.testng.CucumberOptions;
 	plugin = { "json:target/cucumber-report.json", "html:target/cucumber-report", "rerun:rerun/failed_scenarios.txt" }, 
 	features = {"src/test/resources" }, 
 	glue = { "com.orangeHrm.stepDefinitions" }, 
-	tags = System.getProperty("cucumber.filter.tags", "@TC_ADM_006"),
 	dryRun = false,
 	monochrome = true
 	)
