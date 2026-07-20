744c785eff5534d6ece8e9593ad5a930b4393424
geethapriya08 <y.geethapriya@gmail.com>
2026-07-20 09:27:41 +0530

    Signed-off-by: geethapriya08 <y.geethapriya@gmail.com>

--- src/main/java/com/orangeHrm/utils/GitLearning.java
+++ src/main/java/com/orangeHrm/utils/GitLearning.java
@@ -3,6 +3,8 @@ package com.orangeHrm.utils;
 public class GitLearning {
 
     public static void main(String[] args) {
+
         System.out.println("GitLearning");
+        System.out.println("Starting GitLearning");
     }
 }
